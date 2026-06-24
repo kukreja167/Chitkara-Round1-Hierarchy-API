e86167125ec78140a61156b0fcedc971772b70b6
kukreja167 <kavyakukreja167@gmail.com>
2026-06-24 19:12:39 +0530

    updated

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+
+const processHierarchy = require("./hierarchyProcessor");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/bfhl", (req, res) => {
+
+    const data = req.body.data || [];
+
+    const result = processHierarchy(data);
+
+    res.json({
+        user_id: "kavyakukreja_24062026",
+        email_id: "kavya0794.be23@chitkara.edu.in",
+        college_roll_number: "2310990794",
+        ...result
+    });
+});
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port 3000");
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState("");
+
+  const submitData = async () => {
+
+    try {
+
+      setError("");
+
+      const data = input
+        .split(",")
+        .map(item => item.trim())
+        .filter(item => item);
+
+      const response = await axios.post(
+        "http://localhost:3000/bfhl",
+        { data }
+      );
+
+      setResult(response.data);
+
+    } catch (err) {
+
+      setError("Failed to connect to API");
+    }
+  };
+
+  return (
+    <div className="container">
+
+      <h1>Hierarchy Analyzer</h1>
+
+      <textarea
+        rows="10"
+        placeholder="A->B,A->C,B->D"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+
+      <button onClick={submitData}>
+        Analyze
+      </button>
+
+      {error && (
+        <p className="error">{error}</p>
+      )}
+
+      {result && (
+        <pre>
+          {JSON.stringify(result, null, 2)}
+        </pre>
+      )}
+
+    </div>
+  );
+}
+
+export default App;
