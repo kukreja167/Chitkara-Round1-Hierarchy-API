49d7f2f350c29c117486a231ce0235c0f7a4fb13
kukreja167 <kavyakukreja167@gmail.com>
2026-06-24 18:59:17 +0530

    updated

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ app.post("/bfhl", (req, res) => {
         ...result
     });
 });
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Server running on port 3000");
 });
